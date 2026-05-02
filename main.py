eeb4e005c8ab8e6a69edc692c50d7cfb2eabbdf3
rexsy.bima <rexsy.bimq>
2026-05-02 15:47:39 +0700

    Fix QListWidgetItem click handler - extract item.text() instead of object repr

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import (
     QApplication,
+    QLabel,
     QListWidget,
     QMainWindow,
     QPushButton,
     QComboBox,
+    QVBoxLayout,
+    QHBoxLayout,
+    QListWidgetItem,
+    QWidget,
 )
 from src.witcher3_modmenu_changer import (
     IDs,
@@ -24,10 +29,15 @@ xml_files = [f"{f.split('/')[-1]}" for f in retrieve_xml_files()]
 class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        layout = QHBoxLayout()
         self.setFixedSize(QSize(1280, 720))
         self.setWindowTitle("Witcher 3 Modmenu Changer")
+        self.selected_item: None | str = None
         xml_files_list_widget = QListWidget()
         xml_files_list_widget.addItems(xml_files)
+        xml_files_list_widget.currentRowChanged.connect(self.get_item)
+        xml_files_list_widget.setFixedWidth(360)
+        test_label = QLabel("Hello world")
         category_choice = QComboBox()
         category_choice.addItems(IDs2)
         category_choice.currentIndexChanged.connect(self.index_changed)
@@ -36,7 +46,14 @@ class MainApp(QMainWindow):
         button.setCheckable(True)
         button.clicked.connect(self.print_ids)
         button.clicked.connect(self.the_button_was_toggled)
-        self.setCentralWidget(xml_files_list_widget)
+        layout.addWidget(xml_files_list_widget)
+        layout.addWidget(test_label)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def get_item(self, item: int):
+        self.selected_item = xml_files[item]
 
     def index_changed(self, index):
         print(index)
